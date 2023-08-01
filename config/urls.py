f939b446e87358bc6184418d468f164700444ade
Ilia Perfilov <ind.climb@gmail.com>
2023-08-01 12:06:31 +0300

    paths for users and blog were added

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mail_sender.urls', namespace='mailsender')),
+    path('users', include('users.urls', namespace='users')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
 
 if settings.DEBUG:
