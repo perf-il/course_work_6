46f542998102e91bbd8927cc107ea1fdd3958403
Ilia Perfilov <ind.climb@gmail.com>
2023-08-02 13:54:43 +0300

    urls were updated

--- blog/urls.py
+++ blog/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
 
     path('', BlogView.as_view(), name='blog'),
     path('create/', BlogCreateView.as_view(), name='blog_create'),
-    path('<str:slug>/', cache_page(60)(BlogDetailView.as_view()), name='blog_article', ),
-    path('update/<str:slug>/', BlogUpdateView.as_view(), name='blog_update', ),
-    path('delete/<str:slug>/', BlogDeleteView.as_view(), name='blog_delete', ),
+    path('<slug:slug>/', cache_page(60)(BlogDetailView.as_view()), name='blog_article', ),
+    path('update/<slug:slug>/', BlogUpdateView.as_view(), name='blog_update', ),
+    path('delete/<slug:slug>/', BlogDeleteView.as_view(), name='blog_delete', ),
 
 ]
