68190fdccced8ea1f0762b557590bcb7dd43a117
Ilia Perfilov <ind.climb@gmail.com>
2023-08-02 13:55:20 +0300

    path for users was fixed

--- users/urls.py
+++ users/urls.py
@@ -8,15 +8,15 @@ from users.views import RegisterView, ProfileView, wait_confirm_email, ConfirmPa
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('/login', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('/logout', LogoutView.as_view(), name='logout'),
-    path('/register', RegisterView.as_view(), name='register'),
-    path('/profile', ProfileView.as_view(), name='profile'),
-    path('/wait-confirm-email', wait_confirm_email, name='confirm_email'),
-    path('/confirm_email/<int:pk>', ConfirmPage.as_view(), name='confirmed_email'),
-    path('/password-reset/', UserForgotPasswordView.as_view(), name='password_reset'),
-    path('/set-new-password/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('/password-reset-sent/', UserPasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('/password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('wait-confirm-email/', wait_confirm_email, name='confirm_email'),
+    path('confirm_email/<int:pk>', ConfirmPage.as_view(), name='confirmed_email'),
+    path('password-reset/', UserForgotPasswordView.as_view(), name='password_reset'),
+    path('set-new-password/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-sent/', UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
